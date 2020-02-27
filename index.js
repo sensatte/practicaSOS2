7253a0d08ac79c37225e3dd6b2b4d58c0d682d17
root <belenddguez@gmail.com>
2020-02-27 13:29:04 +0000

    app

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-const cool = require("cool-ascii-faces")
+const cool = require("cool-ascii-faces");
+const express = require("express");
+
+var app = express();
+
+app.get("/cool", (request, response) => {
+	response.send("<html>" +cool()+"</html>");
+});
+
+app.listen(80);
 
 
-console.log(cool())
+console.log("readyy",cool());

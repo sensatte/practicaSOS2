f2ad27d92adc83bce51eab3c47f2a6eb47386746
root <belenddguez@gmail.com>
2020-03-20 10:23:30 +0000

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const cool = require("cool-ascii-faces");
 const express = require("express");
+const bodyParser = require("body-parser")
 
 var app = express();
 
+var port = process.env.PORT || 80;
+
+app.use(bodyParser.json);
 //para que podamos usarlo en un html //si hago npm start pero no pongo /cool, me devuelve por defecto el index.html
 app.use("/", express.static("./public"));
 
@@ -10,8 +14,34 @@ app.get("/cool", (request, response) => {
 	response.send("<html>" +cool()+"</html>");
 });
 
-app.listen(80, () => {
-	console.log("readyyyyyyyyy",cool())
+var contacts = [
+	{ 
+		name: "peter",
+		phone: 123456	
+	},
+	{ 
+		name: "pablo",
+		phone: 789456	
+	}
+];
+
+const BASE_API_URL = "/api/v1";
+
+app.get(BASE_API_URL+"/contacts", (req,res) =>{
+	res.send(JSON.stringify(contacts,null,2));
 });
+
+app.post(BASE_API_URL+"/contacts",(req,res) =>{
+	contacts.push(req.body); 
+	res.sendStatus(201,"CREATED");
+});
+
+app.listen(port, () => {
+	console.log("Server ready");
+});
+
+console.log("Starting server...");
 //la cos es que el log de abajo sale depues que lo de arriba, ya que tarda un rato en que el puerto escuche y que te digan que ya sí
 //console.log("readyy",cool());
+
+

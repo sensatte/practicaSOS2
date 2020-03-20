6ed90468cdf37ba621e79064a2d5950c4777b797
root <belendd99@gmail.com>
2020-03-20 11:07:07 +0000

    clase l07

--- index.js
+++ index.js
@@ -1,43 +1,93 @@
-const cool = require("cool-ascii-faces");
 const express = require("express");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 
 var app = express();
 
+app.use(bodyParser.json());
+
 var port = process.env.PORT || 80;
 
-app.use(bodyParser.json);
-//para que podamos usarlo en un html //si hago npm start pero no pongo /cool, me devuelve por defecto el index.html
-app.use("/", express.static("./public"));
-
-app.get("/cool", (request, response) => {
-	response.send("<html>" +cool()+"</html>");
-});
-
 var contacts = [
-	{ 
-		name: "peter",
-		phone: 123456	
-	},
-	{ 
-		name: "pablo",
-		phone: 789456	
-	}
+    { 
+        name: "peter",
+        phone: 123456
+    },
+    { 
+        name: "pablo",
+        phone: 789456
+    }
 ];
 
 const BASE_API_URL = "/api/v1";
 
+// GET CONTACTS
+
 app.get(BASE_API_URL+"/contacts", (req,res) =>{
 	res.send(JSON.stringify(contacts,null,2));
+	console.log("Data sent:"+JSON.stringify(contacts,null,2));
 });
+
+
+// POST CONTACTS
 
 app.post(BASE_API_URL+"/contacts",(req,res) =>{
-	contacts.push(req.body); 
-	res.sendStatus(201,"CREATED");
+	
+	var newContact = req.body;
+	
+	if((newContact == "") || (newContact.name == null)){
+		res.sendStatus(400,"BAD REQUEST");
+	} else {
+		contacts.push(newContact); 	
+		res.sendStatus(201,"CREATED");
+	}
 });
 
+// DELETE CONTACTS
+
+// GET CONTACT/XXX
+
+app.get(BASE_API_URL+"/contacts/:name", (req,res)=>{
+	
+	var name = req.params.name;
+	
+	var filteredContacts = contacts.filter((c) => {
+		return (c.name == name);
+	});
+	
+	
+	if(filteredContacts.length >= 1){
+		res.send(filteredContacts[0]);
+	}else{
+		res.sendStatus(404,"CONTACT NOT FOUND");
+	}
+});
+
+// PUT CONTACT/XXX
+
+// DELETE CONTACT/XXX
+
+app.delete(BASE_API_URL+"/contacts/:name", (req,res)=>{
+	
+	var name = req.params.name;
+	
+	var filteredContacts = contacts.filter((c) => {
+		return (c.name != name);
+	});
+	
+	
+	if(filteredContacts.length < contacts.length){
+		contacts = filteredContacts;
+		res.sendStatus(200);
+	}else{
+		res.sendStatus(404,"CONTACT NOT FOUND");
+	}
+	
+	
+});
+
+
 app.listen(port, () => {
-	console.log("Server ready");
+    console.log("Server ready");
 });
 
 console.log("Starting server...");
